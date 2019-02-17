7e9bc5b2a3a12463016a3b3745336226010b2d29
Andrey <miandmih@gmail.com>
2019-02-17 13:52:44 +0200

    Added timeChart generation, finished lab1

--- src/lab1/app/appLab1.js
+++ src/lab1/app/appLab1.js
@@ -30,18 +30,7 @@ export default class AppLab1 extends Component {
                 name: "",
                 showInLegend: true,
                 dataPoints: [
-                    {y: 155, x: 0},
-                    {y: 150, x: 1},
-                    {y: 152, x: 2},
-                    {y: 148, x: 3},
-                    {y: 142, x: 4},
-                    {y: 150, x: 5},
-                    {y: 146, x: 6},
-                    {y: 149, x: 7},
-                    {y: 153, x: 8},
-                    {y: 158, x: 9},
-                    {y: 154, x: 10},
-                    {y: 150, x: 11}
+                    {y: 0, x: 0},
                 ]
             }]
         },
@@ -66,25 +55,14 @@ export default class AppLab1 extends Component {
                 name: "",
                 showInLegend: true,
                 dataPoints: [
-                    {y: 155, x: 0},
-                    {y: 150, x: 1},
-                    {y: 152, x: 2},
-                    {y: 148, x: 3},
-                    {y: 142, x: 4},
-                    {y: 150, x: 5},
-                    {y: 146, x: 6},
-                    {y: 149, x: 7},
-                    {y: 153, x: 8},
-                    {y: 158, x: 9},
-                    {y: 154, x: 10},
-                    {y: 150, x: 11}
+                    {y: 0, x: 0},
                 ]
             }]
         },
         optionsTime: {
             animationEnabled: false,
             title: {
-                text: "Generated signal timeChart"
+                text: "Dependence of time on the number of discrete samples"
             },
             axisY: {
                 title: "Y",
@@ -98,22 +76,32 @@ export default class AppLab1 extends Component {
                 shared: true
             },
             data: [{
-                type: "line",
-                name: "",
+                type: "spline",
+                name: "Number of discrete samples is 2^X",
                 showInLegend: true,
                 dataPoints: [
-                    {y: 155, x: 0},
-                    {y: 150, x: 1},
-                    {y: 152, x: 2},
-                    {y: 148, x: 3},
-                    {y: 142, x: 4},
-                    {y: 150, x: 5},
-                    {y: 146, x: 6},
-                    {y: 149, x: 7},
-                    {y: 153, x: 8},
-                    {y: 158, x: 9},
-                    {y: 154, x: 10},
-                    {y: 150, x: 11}
+                    {x: 0, y: 0.2549999999246211},
+                    {x: 1, y: 0.045000000227446435},
+                    {x: 2, y: 0.06499999972220394},
+                    {x: 3, y: 0.105000000075961},
+                    {x: 4, y: 0.20500000027823262},
+                    {x: 5, y: 0.37500000007639755},
+                    {x: 6, y: 0.7800000003044261},
+                    {x: 7, y: 4.380000000310247},
+                    {x: 8, y: 1.4449999998760177},
+                    {x: 9, y: 9.299999999711872},
+                    {x: 10, y: 1.6550000000279397},
+                    {x: 11, y: 3.23000000025786},
+                    {x: 12, y: 6.530000000111613},
+                    {x: 13, y: 13.669999999819993},
+                    {x: 14, y: 27.61000000009517},
+                    {x: 15, y: 52.16500000005908},
+                    {x: 16, y: 107.05000000007203},
+                    {x: 17, y: 209.19000000003507},
+                    {x: 18, y: 425.2549999996518},
+                    {x: 19, y: 834.8650000002635},
+                    {x: 20, y: 1687.7549999999246},
+                    {x: 21, y: 3263.1200000000717}
                 ]
             }]
         },
@@ -135,38 +123,26 @@ export default class AppLab1 extends Component {
         let signalChart = [];
         let harmonicCharts = [];
 
-
-        const start = new Date().getMilliseconds();
+        const start = window.performance.now();
         this.generateHarmonics(harmonicsNum, harmonicCharts, division, discreteSamples);
         this.generateSignal(harmonicCharts, signalChart);
-        const end = new Date().getMilliseconds();
+        const end = window.performance.now();
 
-        console.log(harmonicCharts);
-        console.log(signalChart);
+        //console.log(harmonicCharts);
+        //console.log(signalChart);
 
         const expectation = this.mathExpectation(signalChart);
         const dispersion = this.mathDispersion(signalChart, expectation);
 
-       /* let theoreticStart = 0;
-        let theoreticEnd = 0;
-        let timeChart = [];
-        let count = 0;
+        /*//Uncomment this code if you want to rebuild the timeChart!!
+        let timeChart = this.buildTimeChart(harmonicsNum, division, 24);*/
 
-        while ((theoreticEnd - theoreticStart) < 150){
-            theoreticStart = new Date().getMilliseconds();
-            this.generateSignalTrue(harmonicsNum, division, discreteSamples);
-            theoreticEnd = new Date().getMilliseconds();
-            timeChart[count] = (theoreticEnd - theoreticStart);
-            count++;
-        }
-
-        console.log(timeChart);*/
 
         this.setState(({optionsHarmonic, optionsFull, optionsTime}) => {
 
             let newOptionsHarmonic = JSON.parse(JSON.stringify(optionsHarmonic));
             let newOptionsFull = JSON.parse(JSON.stringify(optionsFull));
-           // let newOptionsTime = JSON.parse(JSON.stringify(optionsTime));
+            let newOptionsTime = JSON.parse(JSON.stringify(optionsTime));
 
             newOptionsFull.data[0] = {
                 type: "line",
@@ -175,14 +151,18 @@ export default class AppLab1 extends Component {
                 dataPoints: this.arrToObjects(signalChart)
             };
 
-            for (let i = 0; i < harmonicsNum; i++){
+            console.log(harmonicCharts);
+
+            for (let i = 0; i < harmonicsNum; i++) {
                 newOptionsHarmonic.data[i] = {
                     type: "spline",
-                    name: `${i+1}`,
+                    name: `${i + 1}`,
                     showInLegend: true,
                     dataPoints: this.arrToObjects(harmonicCharts[i])
                 }
             }
+
+            newOptionsHarmonic.data.splice(harmonicsNum, newOptionsHarmonic.data.length);
 
             let newResults = {
                 expectation: expectation,
@@ -190,29 +170,22 @@ export default class AppLab1 extends Component {
                 time: (end - start)
             };
 
+            /*newOptionsTime.data[0] = {
+                type: "spline",
+                name: "Number of discrete samples is 2^X",
+                showInLegend: true,
+                dataPoints: timeChart
+            };*/
+
 
             return {
                 optionsHarmonic: newOptionsHarmonic,
                 optionsFull: newOptionsFull,
-                //optionsTime: newOptionsTime,
+                optionsTime: newOptionsTime,
                 results: newResults,
             };
         })
     };
-
-    /*generateSignalTrue = (harmonicsNum, division, discreteSamples) => {
-        let chart = [];
-        for (let i = 0; i < discreteSamples; i++) {
-            let sum = 0;
-            let amplitude = Math.random();
-            let phase = Math.random() * 2 * Math.PI;
-            for (let j = 0; j < harmonicsNum; j++) {
-                sum += amplitude * Math.sin(division * (j + 1) + phase);
-            }
-            chart[i] = sum;
-        }
-    };*/
-
 
     generateSingleHarmonic = (division, discreteSamples) => {
         const chart = [];
@@ -271,15 +244,47 @@ export default class AppLab1 extends Component {
         return sum / (arr.length - 1);
     };
 
+    generateSignalTrue = (harmonicsNum, division, discreteSamples) => {
+        let chart = [];
+        for (let i = 0; i < discreteSamples; i++) {
+            let sum = 0;
+            let amplitude = Math.random();
+            let phase = Math.random() * 2 * Math.PI;
+            for (let j = 0; j < harmonicsNum; j++) {
+                sum += amplitude * Math.sin(division * (j + 1) + phase);
+            }
+            chart[i] = sum;
+        }
+    };
+
+    buildTimeChart = (harmonicsNum, division, power) => {
+        let theoreticStart = 0;
+        let theoreticEnd = 0;
+        let timeChart = [];
+        let sample = 2;
+
+        for (let i = 0; i <= power; i++) {
+            theoreticStart = window.performance.now();
+            this.generateSignalTrue(harmonicsNum, division, sample);
+            theoreticEnd = window.performance.now();
+            timeChart[i] = {x: i, y: (theoreticEnd - theoreticStart)};
+            sample = sample * 2;
+        }
+        console.log(timeChart);
+        return timeChart;
+    };
+
+
     render() {
         return (
             <div className={'main'}>
-                <div className={"d-flex top"}>
+                <div className={"d-flex"}>
                     <Form onValuesPushed={this.update}/>
                     <Result results={this.state.results}/>
                 </div>
                 <CanvasJSChart options={this.state.optionsFull}/>
                 <CanvasJSChart options={this.state.optionsHarmonic}/>
+                <CanvasJSChart options={this.state.optionsTime}/>
             </div>
         );
     }
--- src/lab1/result/result.js
+++ src/lab1/result/result.js
@@ -12,7 +12,7 @@ export default class Result extends Component {
                 <ul className={"list-group"}>
                     <li className={"list-group-item"}>Math expectation is: {results.expectation.toFixed(4)}</li>
                     <li className={"list-group-item"}>Dispersion is: {results.dispersion.toFixed(4)}</li>
-                    <li className={"list-group-item"}>Time is: {results.time} (ms)</li>
+                    <li className={"list-group-item"}>Time is: {results.time.toFixed(4)} (ms)</li>
                 </ul>
             </div>
 
